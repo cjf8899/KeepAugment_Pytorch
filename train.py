ff105248b64c3777087f425e497d0ac273efaf12
Hyoencheol <53032349+cjf8899@users.noreply.github.com>
2022-06-03 18:36:51 +0900

    KeepAugment code upload

---
+++ train.py
@@ -0,0 +1,191 @@
+
+import pdb
+import argparse
+import numpy as np
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.backends.cudnn as cudnn
+from torch.optim.lr_scheduler import MultiStepLR
+
+from torchvision.utils import make_grid
+from torchvision import datasets, transforms
+
+from util.misc import CSVLogger
+from util.keep_cutout import Cutout, Keep_Cutout, Keep_Cutout_Low
+from util.keep_autoaugment import RandAugment, Keep_Autoaugment, Keep_Autoaugment_Low
+
+from model.resnet import ResNet18
+from model.wide_resnet import WideResNet
+from model.shake_resnet import ShakeResNet
+
+
+model_options = ['resnet', 'wideresnet', 'shake']
+dataset_options = ['cifar10']
+method_options = ['none', 'cutout', 'keep_cutout', 'keep_cutout_low',  
+                   'autoaugment','keep_autoaugment','keep_autoaugment_low']
+
+parser = argparse.ArgumentParser(description='CNN')
+parser.add_argument('--dataset', '-d', default='cifar10', choices=dataset_options)
+parser.add_argument('--model', '-a', default='resnet', choices=model_options)
+parser.add_argument('--batch_size', type=int, default=128)
+parser.add_argument('--epochs', type=int, default=300)
+parser.add_argument('--learning_rate', type=float, default=0.1)
+parser.add_argument('--length', type=int, default=16, help='length of the holes')
+parser.add_argument('--N', type=int, default=3, help='number of autoaugments')
+parser.add_argument('--M', type=int, default=24, help='magnitude of autoaugments')
+parser.add_argument('--no-cuda', action='store_true', default=False, help='enables CUDA training')
+parser.add_argument('--seed', type=int, default=0, help='random seed (default: 1)')
+parser.add_argument('--method', default='none', choices=method_options)
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+cudnn.benchmark = True  # Should make training should go faster for large models
+
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+test_id = args.dataset + '_' + args.model + '_' + args.method
+
+print(args)
+
+# Image Preprocessing
+mean=[x / 255.0 for x in [125.3, 123.0, 113.9]]
+std=[x / 255.0 for x in [63.0, 62.1, 66.7]]
+normalize = transforms.Normalize(mean, std)
+
+
+train_transform = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    normalize])
+    
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    normalize])
+
+if args.method=='cutout':
+    train_transform.transforms.append(Cutout(n_holes=1, length=args.length))
+elif args.method=='autoaugment':
+    train_transform.transforms.insert(0, RandAugment(args.N, args.M))
+elif args.method=='keep_cutout':
+    keep = Keep_Cutout(train_transform, mean, std, args.length)
+elif args.method=='keep_cutout_low':
+    keep = Keep_Cutout_Low(train_transform, mean, std, args.length)
+elif args.method=='keep_autoaugment':
+    keep = Keep_Autoaugment(train_transform, mean, std, args.length, args.N, args.M)
+elif args.method=='keep_autoaugment_low':
+    keep = Keep_Autoaugment_Low(train_transform, mean, std, args.length, args.N, args.M)
+
+
+
+train_dataset = datasets.CIFAR10(root='data/',
+                                 train=True,
+                                 transform=train_transform,
+                                 download=True)
+
+test_dataset = datasets.CIFAR10(root='data/',
+                                train=False,
+                                transform=test_transform,
+                                download=True)
+
+
+# Data Loader (Input Pipeline)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=args.batch_size,
+                                           shuffle=True,
+                                           pin_memory=True,
+                                           num_workers=8)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=args.batch_size,
+                                          shuffle=False,
+                                          pin_memory=True,
+                                          num_workers=8)
+
+if args.model == 'shake':
+    cnn = ShakeResNet(depth=26, w_base=32, label=10)
+elif args.model == 'wideresnet':
+    cnn = WideResNet(depth=28, num_classes=10, widen_factor=10,dropRate=0.3)
+else:
+    cnn = ResNet18(num_classes=10)
+
+
+cnn = cnn.cuda()
+criterion = nn.CrossEntropyLoss().cuda()
+cnn_optimizer = torch.optim.SGD(cnn.parameters(), lr=args.learning_rate,
+                                momentum=0.9, nesterov=True, weight_decay=5e-4)
+scheduler = MultiStepLR(cnn_optimizer, milestones=[60, 120, 160], gamma=0.2)
+
+filename = 'logs/' + test_id + '.csv'
+csv_logger = CSVLogger(args=args, fieldnames=['epoch', 'train_acc', 'test_acc'], filename=filename)
+
+
+def test(loader):
+    cnn.eval()    # Change model to 'eval' mode (BN uses moving mean/var).
+    correct = 0.
+    total = 0.
+    for images, labels in loader:
+        images = images.cuda()
+        labels = labels.cuda()
+
+        with torch.no_grad():
+            pred = cnn(images)
+
+        pred = torch.max(pred.data, 1)[1]
+        total += labels.size(0)
+        correct += (pred == labels).sum().item()
+
+    val_acc = correct / total
+    cnn.train()
+    return val_acc
+
+
+for epoch in range(args.epochs):
+
+    xentropy_loss_avg = 0.
+    correct = 0.
+    total = 0.
+
+    progress_bar = tqdm(train_loader)
+    for i, (images, labels) in enumerate(progress_bar):
+        progress_bar.set_description('Epoch ' + str(epoch))
+
+        images = images.cuda()
+        labels = labels.cuda()
+        if 'keep' in args.method: 
+            images = keep(images, cnn)
+
+        cnn.zero_grad()
+        pred = cnn(images)
+
+        xentropy_loss = criterion(pred, labels)
+        xentropy_loss.backward()
+        cnn_optimizer.step()
+
+        xentropy_loss_avg += xentropy_loss.item()
+
+        # Calculate running average of accuracy
+        pred = torch.max(pred.data, 1)[1]
+        total += labels.size(0)
+        correct += (pred == labels.data).sum().item()
+        accuracy = correct / total
+
+        progress_bar.set_postfix(
+            xentropy='%.3f' % (xentropy_loss_avg / (i + 1)),
+            acc='%.3f' % accuracy)
+
+    test_acc = test(test_loader)
+    tqdm.write('test_acc: %.3f' % (test_acc))
+
+    scheduler.step(epoch)  # Use this line for PyTorch <1.4
+    # scheduler.step()     # Use this line for PyTorch >=1.4
+
+    row = {'epoch': str(epoch), 'train_acc': str(accuracy), 'test_acc': str(test_acc)}
+    csv_logger.writerow(row)
+
+torch.save(cnn.state_dict(), 'checkpoints/' + test_id + '.pt')
+csv_logger.close()
---
+++ util/keep_cutout.py
@@ -0,0 +1,170 @@
+import torch
+import numpy as np
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+
+class Keep_Cutout_Low(object):
+    """Randomly mask out one or more patches from an image.
+    Args:
+        n_holes (int): Number of patches to cut out of each image.
+        length (int): The length (in pixels) of each square patch.
+    """
+    def __init__(self, train_transform, mean, std, length):
+        self.trans = train_transform
+        self.length = int(length/2)
+        self.denomal = transforms.Compose([
+            transforms.Normalize((-mean[0]/std[0],-mean[1]/std[1], -mean[2]/std[2]), (1/std[0], 1/std[1], 1/std[2])),
+            transforms.ToPILImage()
+        ])
+
+    def __call__(self, images, model):
+        for param in model.parameters():
+            param.requires_grad = False
+        model.eval()
+        images_ = images.clone().detach()
+        images_half = F.interpolate(images_, scale_factor=0.5, mode='bicubic',align_corners=True)
+        images_half.requires_grad = True
+
+        preds = model(images_half)
+        
+        score, _ = torch.max(preds, 1)
+        score.mean().backward()
+        slc_, _ = torch.max(torch.abs(images_half.grad), dim=1)
+        
+        b,h,w = slc_.shape
+        
+        slc_ = slc_.view(slc_.size(0), -1)
+        slc_ -= slc_.min(1, keepdim=True)[0]
+        slc_ /= slc_.max(1, keepdim=True)[0]
+        slc_ = slc_.view(b, h, w)
+        
+        
+        for i,(img, slc) in enumerate(zip(images_, slc_)):
+            mask = np.ones((h*2, w*2), np.float32)
+            while(True):
+                y = np.random.randint(h)
+                x = np.random.randint(w)
+
+                y1 = np.clip(y - self.length // 2, 0, h)
+                y2 = np.clip(y + self.length // 2, 0, h)
+                x1 = np.clip(x - self.length // 2, 0, w)
+                x2 = np.clip(x + self.length // 2, 0, w)
+
+                if slc[y1: y2, x1: x2].mean() < 0.6:
+                    mask[y1*2: y2*2, x1*2: x2*2] = 0.
+                    break
+
+            mask = torch.from_numpy(mask)
+            mask = mask.expand_as(img).cuda()
+            img = img * mask
+            images[i] = self.trans(self.denomal(img))
+                
+        model.train()
+        for param in model.parameters():
+            param.requires_grad = True
+#         for img in images:
+#             save_image(img, 'test.jpg')
+        return images.cuda()
+
+
+class Keep_Cutout(object):
+    """Randomly mask out one or more patches from an image.
+    Args:
+        n_holes (int): Number of patches to cut out of each image.
+        length (int): The length (in pixels) of each square patch.
+    """
+    def __init__(self, train_transform, mean, std, length):
+        self.trans = train_transform
+        self.length = length
+        self.denomal = transforms.Compose([
+            transforms.Normalize((-mean[0]/std[0],-mean[1]/std[1], -mean[2]/std[2]), (1/std[0], 1/std[1], 1/std[2])),
+            transforms.ToPILImage()
+        ])
+        
+    def __call__(self, images, model):
+        for param in model.parameters():
+            param.requires_grad = False
+        model.eval()
+        images_ = images.clone().detach()
+        images_.requires_grad = True
+
+        preds = model(images_)
+
+        score, _ = torch.max(preds, 1)
+        score.mean().backward()
+        slc_, _ = torch.max(torch.abs(images_.grad), dim=1)
+        
+        b,h,w = slc_.shape
+        
+        slc_ = slc_.view(slc_.size(0), -1)
+        slc_ -= slc_.min(1, keepdim=True)[0]
+        slc_ /= slc_.max(1, keepdim=True)[0]
+        slc_ = slc_.view(b, h, w)
+        
+        for i,(img, slc) in enumerate(zip(images_, slc_)):
+            mask = np.ones((h, w), np.float32)
+            while(True):
+                y = np.random.randint(h)
+                x = np.random.randint(w)
+
+                y1 = np.clip(y - self.length // 2, 0, h)
+                y2 = np.clip(y + self.length // 2, 0, h)
+                x1 = np.clip(x - self.length // 2, 0, w)
+                x2 = np.clip(x + self.length // 2, 0, w)
+
+                if slc[y1: y2, x1: x2].mean() < 0.6:
+                    mask[y1: y2, x1: x2] = 0.
+                    break
+
+            mask = torch.from_numpy(mask)
+            mask = mask.expand_as(img).cuda()
+            img = img * mask
+            images[i] = self.trans(self.denomal(img))
+                
+        model.train()
+        for param in model.parameters():
+            param.requires_grad = True
+            
+#         for img in images:
+#             save_image(img, 'test.jpg')
+        return images.cuda()
+
+class Cutout(object):
+    """Randomly mask out one or more patches from an image.
+
+    Args:
+        n_holes (int): Number of patches to cut out of each image.
+        length (int): The length (in pixels) of each square patch.
+    """
+    def __init__(self, n_holes, length):
+        self.n_holes = n_holes
+        self.length = length
+
+    def __call__(self, img):
+        """
+        Args:
+            img (Tensor): Tensor image of size (C, H, W).
+        Returns:
+            Tensor: Image with n_holes of dimension length x length cut out of it.
+        """
+        h = img.size(1)
+        w = img.size(2)
+
+        mask = np.ones((h, w), np.float32)
+
+        for n in range(self.n_holes):
+            y = np.random.randint(h)
+            x = np.random.randint(w)
+
+            y1 = np.clip(y - self.length // 2, 0, h)
+            y2 = np.clip(y + self.length // 2, 0, h)
+            x1 = np.clip(x - self.length // 2, 0, w)
+            x2 = np.clip(x + self.length // 2, 0, w)
+
+            mask[y1: y2, x1: x2] = 0.
+
+        mask = torch.from_numpy(mask)
+        mask = mask.expand_as(img)
+        img = img * mask
+
+        return img
