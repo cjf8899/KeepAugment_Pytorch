10901171a20933a11c857c391a88419c019f3e87
Hyoencheol <53032349+cjf8899@users.noreply.github.com>
2022-06-06 16:25:43 +0900

    code update

--- util/keep_cutout.py
+++ util/keep_cutout.py
@@ -5,11 +5,6 @@ import torch.nn.functional as F
 
 
 class Keep_Cutout_Low(object):
-    """Randomly mask out one or more patches from an image.
-    Args:
-        n_holes (int): Number of patches to cut out of each image.
-        length (int): The length (in pixels) of each square patch.
-    """
     def __init__(self, train_transform, mean, std, length, early=False):
         self.trans = train_transform
         self.length = int(length/2)
@@ -35,11 +30,6 @@ class Keep_Cutout_Low(object):
         score, _ = torch.max(preds, 1)
         score.mean().backward()
         slc_, _ = torch.max(torch.abs(images_half.grad), dim=1)
-        
-#         slc_ = torch.unsqueeze(slc_,dim=1)
-#         slc_ = F.upsample(slc_, size=(slc_.shape[-2]*2,slc_.shape[-1]*2), mode='bicubic')
-#         slc_ = torch.squeeze(slc_,dim=1)
-        
         
         b,h,w = slc_.shape
         slc_ = slc_.view(slc_.size(0), -1)
@@ -70,17 +60,10 @@ class Keep_Cutout_Low(object):
         model.train()
         for param in model.parameters():
             param.requires_grad = True
-#         for img in images:
-#             save_image(img, 'test.jpg')
         return images.cuda()
 
 
 class Keep_Cutout(object):
-    """Randomly mask out one or more patches from an image.
-    Args:
-        n_holes (int): Number of patches to cut out of each image.
-        length (int): The length (in pixels) of each square patch.
-    """
     def __init__(self, train_transform, mean, std, length, early=False):
         self.trans = train_transform
         self.length = length
@@ -136,47 +119,5 @@ class Keep_Cutout(object):
         model.train()
         for param in model.parameters():
             param.requires_grad = True
-            
-#         for img in images:
-#             save_image(img, 'test.jpg')
         return images.cuda()
 
-class Cutout(object):
-    """Randomly mask out one or more patches from an image.
-
-    Args:
-        n_holes (int): Number of patches to cut out of each image.
-        length (int): The length (in pixels) of each square patch.
-    """
-    def __init__(self, n_holes, length):
-        self.n_holes = n_holes
-        self.length = length
-
-    def __call__(self, img):
-        """
-        Args:
-            img (Tensor): Tensor image of size (C, H, W).
-        Returns:
-            Tensor: Image with n_holes of dimension length x length cut out of it.
-        """
-        h = img.size(1)
-        w = img.size(2)
-
-        mask = np.ones((h, w), np.float32)
-
-        for n in range(self.n_holes):
-            y = np.random.randint(h)
-            x = np.random.randint(w)
-
-            y1 = np.clip(y - self.length // 2, 0, h)
-            y2 = np.clip(y + self.length // 2, 0, h)
-            x1 = np.clip(x - self.length // 2, 0, w)
-            x2 = np.clip(x + self.length // 2, 0, w)
-
-            mask[y1: y2, x1: x2] = 0.
-
-        mask = torch.from_numpy(mask)
-        mask = mask.expand_as(img)
-        img = img * mask
-
-        return img
